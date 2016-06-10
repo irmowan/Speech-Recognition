8b66b26f66b99d3ec04d4ef6bdbbf16821b41208
irmowan <irmowan@gmail.com>
2016-06-10 21:09:28 +0800

    Add mdcc

---
+++ mfcc.m
@@ -0,0 +1,61 @@
+% function ccc = mfcc(~)
+
+% 归一化mel滤波器组系数
+
+% fs: 采样频率
+[x, fs] = audioread(['SpeechDataset/', sid, '/', sid, '_', word, '_', no, '.wav']);
+
+% melbankm: 
+% melbankm: 最后一个参数，'t'是三角窗,'n'是hanning窗，'m'是hamming窗
+bank = melbankm(24, 256, fs, 0, 0.4, 't');
+bank = full(bank);          % 将稀疏矩阵转换成完整的矩阵
+bank = bank/max(bank(:));   % 归一化
+
+% 产生离散余弦变换的参数
+for k = 1:12
+    n = 0:23;
+    dctcoef(k, :) = cos((2*n+1)*k*pi/(2*24));
+end
+
+% 归一化倒谱提升窗口
+w = 1+6*sin(pi*(1:12)./12);
+w = w/max(w);
+
+% 预加重滤波器
+xx = filter([1 -0.9375], 1, double(x));
+
+% 语音信号分帧
+xx = enframe(xx, 256, 80);
+
+% MFCC计算的核心部分
+% 遍历每帧,计算MFCC参数
+for i = 1:size(xx,1)
+    y = xx(i,:);
+    s = y'.*hamming(256);
+    t = abs(fft(s));
+    t = t.^2;
+    c1= dctcoef*log(bank*t(1:129));
+    c2= c1.*w';
+    m(i,:)=c2';
+end
+
+% 差分参数
+dtm=zeros(size(m));
+for i = 3:size(m,1)-2
+    dtm(i,:) = -2*m(i-2,:)-m(i-1,:)+m(i+1,:)+2*m(i+2,:);
+end
+
+dtm = dtm/3;
+
+% 合并mfcc参数和一阶差分mfcc参数
+ccc = [m dtm];
+
+% 去除首位两帧，因为这两帧的一阶差分参数为0
+ccc = ccc(3:size(m,1)-2,:);
+
+subplot(211)
+ccc_1 = ccc(:,1);
+plot(ccc);
+title('MFCC');
+
+% return
