09c4ba4295131d6c354d7259579ea7d5d2f4387e
irmowan <irmowan@gmail.com>
2016-06-27 17:20:41 +0800

    Add endpoint detection results
    批量生成端点检测图像并导出

--- endpoint_detection.m
+++ endpoint_detection.m
@@ -1,115 +1,33 @@
-% endpoint_detection: 端点检测
-% author: irmo
-% date: 2016.6
-function [x1, x2] = endpoint_detection(f)
-% fs: 采样频率
+% endpoint_detetion
+% 批量导出端点检测图像
+% 注，此处仅导出no='01'的图像，供展示使用
+clear all;
+clc;
 
-[x, fs] = audioread(f);
-
-% 播放音频
-soundsc(x, fs);
-xxx=x;
-
-% 小波降噪
-[thr, sorh, keepapp] = ddencmp('den','wv',x(:));
-x = wdencmp('gbl',x,'coif5',5,thr,'s',keepapp);
-N = size(x);
-x = double(x);
-x = x/max(abs(x)); % 归一化
-
-% 确定帧长、帧移并切割
-FrameLen = 240;
+load('ids.mat');
+load('words.mat');
 FrameInc = 80;
-tmp1  = enframe(x(1:end-1), FrameLen, FrameInc);
-tmp2  = enframe(x(2:end)  , FrameLen, FrameInc);
+FrameLen = 256;
 
-signs = (tmp1.*tmp2) < 0;
-diffs = (tmp1 -tmp2) > 0.02;
-
-% zcr: 过零率
-% amp: 短时能量
-% ampH: 短时能量 高阈值
-% ampL: 短时能量 低阈值ֵ
-zcr   = sum(signs.*diffs, 2);   
-% zcrH = 5;
-zcrL = 1;
-amp = sum(abs(enframe(filter([1 -0.9375],1,x), FrameLen, FrameInc)),2);
-amp1 = 2;
-amp2 = 1;
-ampH = min(amp1, max(amp)/4);
-ampL = min(amp2, max(amp)/8);
-
-% status: 状态
-% minlen: 最小检测长度
-% maxsilence: 最长允许静音长度
-% count: 有效语音长度
-% silence: 静音长度
-maxsilence = 40;
-minlen = 30;
-status = 0;
-count = 0;
-silence = 0;
-
-% 端点检测
-x1 = 0;
-for n = 1 : length(zcr)
-    switch status
-        % status = 0, 静音
-        % status = 1, 可能开始
-        % status = 2, 确认开始
-        % status = 3, 结束
-        case {0,1}
-            if amp(n) > ampH
-                x1 = max(n-count-1, 1);
-                status = 2;
-                silence = 0;
-                count = count + 1;
-            elseif amp(n) > ampL || zcr(n) > zcrL
-                status = 1;
-                count = count + 1;
-            else
-                status = 0;
-                count = 0;
-            end
-        case 2,
-            if amp(n) > ampL || zcr(n) > zcrL
-                count = count + 1;
-            else
-                silence = silence + 1;
-                if silence < maxsilence
-                    count = count + 1;
-                elseif count < minlen
-                    status = 0;
-                    silence = 0;
-                    count = 0;
-                else
-                    status = 3;
-                end
-            end
-        case 3,
-            break;
+for i = 1:num_words
+    word = words{i};
+    for j = 1:num_ids
+        id = ids{j};
+        
+        fname = generate_fname(id, word, '01');
+        if exist(fname,'file') == 0
+            continue
+        end
+        
+        [x, fs] = audioread(fname);
+        fprintf('处理%s  %s...\n', id, word);
+        [x1, x2] = endpoint_detect(x, fs);
+        
+        plot(x,'b');
+        title('时域波形图');
+        hold on
+        plot([x1*FrameInc x1*FrameInc], [-1 1], 'r', [x2*FrameInc+FrameLen x2*FrameInc+FrameLen], [-1, 1], 'r');
+        saveas(gcf, strcat(pwd,'/results/endpoint/',id, '_', word),'jpg');
+        clf;
     end
 end
-count = count - silence / 2;
-x2 = x1 + count - 1;
-result = [x1, x2];
-
-figure(1);
-
-subplot(311);
-plot(xxx,'b')
-title('时域波形图')
-hold on
-plot([x1*FrameInc x1*FrameInc], [-1 1], 'r', [x2*FrameInc+FrameLen x2*FrameInc+FrameLen], [-1, 1], 'r');
-
-subplot(312);
-plot(zcr);
-title('有效过零率')
-xlabel('֡帧')
-
-subplot(313);
-plot(amp);
-title('短时能量');
-xlabel('帧');
-
-return 
